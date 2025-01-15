acfb747089781e593514ca9a26f305124b7d2675
Ha-Ree <48283886+Ha-Ree@users.noreply.github.com>
2025-01-15 21:52:51 +0000

    Refactor

--- HurricaneSimulator.py
+++ HurricaneSimulator.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import numpy as np
 from LogNormalCLT import log_normal_CLT
 import logging
@@ -28,30 +29,30 @@ class HurricaneSimulator:
         ValueError: If rate is not positive.
         ValueError: If stddev is negative.
     """
-    def __init__(self, rate: float, mean: float, stddev: float):
-        if (rate <= 0):
+    def __init__(self, rate: float, mean: float, stddev: float) -> None:
+        if rate <= 0:
             rate_error_message = f"Rate must be positive. Rate given: {rate}."
             logger.error(rate_error_message)
             raise ValueError(rate_error_message)
-        if (stddev < 0):
+        if stddev < 0:
             stddev_error_message = f"Standard deviation must be non-negative. Deviation given: {stddev}"
             logger.error(stddev_error_message)
             raise ValueError(stddev_error_message)
         self.rate = rate
         self.mean = mean
         self.stddev = stddev
-        logger.info(f"HurricaneSimulation initialized with rate={rate}, mean={mean}, stddev={stddev}.")
+        logger.info(f"HurricaneSimulation initialised with rate={rate}, mean={mean}, stddev={stddev}.")
     
-    def simulate(self, years: int, batch_size: int = 1000, use_CLT: bool = False) -> float:
+    def simulate(self, years: int, batch_size: Optional[int] = 1000, use_CLT: Optional[bool] = False) -> float:
         """
         Simulates the total economic loss from hurricanes over a given number of years.
 
         Parameters:
             years: int
                 The number of years to simulate over.
-            batch_size: int, optional
+            batch_size: Optional[int]
                 The maximum number of events to process in a single batch to conserve memory. Default is 1000.
-            use_CLT: bool, optional
+            use_CLT: Optional[bool]
                 Whether to directly simulate or to use the Central Limit Theorem for efficiency. Default is False.
 
         Returns:
@@ -62,7 +63,7 @@ class HurricaneSimulator:
         num_events = np.random.poisson(rate, 1)[0]
         logger.info(f"Simulating {num_events} hurricanes.")
         if use_CLT:
-            logger.info(f"Simulating using CLT.")
+            logger.info("Simulating using CLT.")
             total_loss = log_normal_CLT(self.mean, self.stddev, num_events)
             logger.info(f"Finished simulating. Total loss: {total_loss}.")
             return total_loss
--- Simulation.py
+++ Simulation.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from HurricaneSimulator import HurricaneSimulator
 import logging
 
@@ -33,32 +34,32 @@ class Simulation:
         ValueError: If gulf_landfall_rate is not positive.
         ValueError: If gulf_stddev is not positive.
     """
-    def __init__(self, florida_landfall_rate: float, florida_mean: float, florida_stddev: float, gulf_landfall_rate: float, gulf_mean: float, gulf_stddev: float):        
+    def __init__(self, florida_landfall_rate: float, florida_mean: float, florida_stddev: float, gulf_landfall_rate: float, gulf_mean: float, gulf_stddev: float) -> None:        
         self.florida = HurricaneSimulator(florida_landfall_rate, florida_mean, florida_stddev)
         self.gulf = HurricaneSimulator(gulf_landfall_rate, gulf_mean, gulf_stddev)
         logger.info("Simulation initialised.")
     
-    def simulate(self, years: int, use_CLT: bool = False) -> float:
+    def simulate(self, years: int, use_CLT: Optional[bool] = False) -> float:
         """
         Simulates the average economic loss over a given number of years.
 
         Parameters:
             years: int
                 The number of years to simulate over.
-            use_CLT: bool, optional
+            use_CLT: Optional[bool]
                 whether to use the Central Limit Theorem (CLT) to speed up simulation. Default is False.
 
         Returns:
             float
                 The expected annual loss in billions of dollars.
         """
-        if (years <= 0):
-            logger.info(f"Simulation for no time.")
+        if years <= 0:
+            logger.info("Simulation for no time.")
             return 0
         
         logger.info(f"Starting simulation for {years} years.")
         
-        florida_loss = self.florida.simulate(years, use_CLT=use_CLT) #More efficient to simulate all at once than to loop over years with no/low hurricanes.
+        florida_loss = self.florida.simulate(years, use_CLT=use_CLT) # More efficient to simulate all at once than to loop over years with no/low hurricanes.
         logger.info(f"Florida simulation completed. Florida loss: {florida_loss:.2f} billion.")
         
         gulf_loss = self.gulf.simulate(years, use_CLT=use_CLT)
